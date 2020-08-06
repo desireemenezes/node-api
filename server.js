4674f99fc3f264bb6a4d46b2cecfb0e8869a061a
desireemenezes <deemenezes@hotmail.com>
2020-08-05 22:35:31 -0300

    add moongose

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
+//init app
 const app = express()
 const port = 3000
 
+// init Db
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+
+// first route
 app.get('/', (req, res) => {
     res.send('Hello World! ')
   })
